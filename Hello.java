5e6a6ec621d6f96e51fd15f102fcb28658b57e44
Sanjeev Rai <sanjeevkrai1@gmail.com>
2021-06-06 06:17:59 +0000

    Updated Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 class Hello{
 	public static void main(String args[]){
-		System.out.println("Hello world from chlid");	
+		System.out.println("Hello world from chlid");
+		System.out.println("Hello world from feature-101 branch");	
+		HelloFeature.helloFeature();
 	}
 }
