f5518dfbc1f39efd5b18125326c7f598af92555a
Sanjeev Rai <sanjeevkrai1@gmail.com>
2021-06-06 07:33:15 +0000

    resolved changes

--- Hello.java
+++ Hello.java
@@ -1,7 +1,6 @@
 class Hello{
 	public static void main(String args[]){
 		System.out.println("Hello world from master");
-
 		System.out.println("by mistake changes done");
 		System.out.println("Hello world from feature-101 branch");	
 		HelloFeature.helloFeature();
