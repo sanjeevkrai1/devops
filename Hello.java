486e869f150fa19324145093e225a866d9bb0ee8
Sanjeev Rai <sanjeevkrai1@gmail.com>
2021-06-06 06:45:05 +0000

    changes in master

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello{
 	public static void main(String args[]){
-		System.out.println("Hello world from chlid");
+		System.out.println("Hello world from master");
 		System.out.println("Hello world from feature-101 branch");	
 		HelloFeature.helloFeature();
 	}
